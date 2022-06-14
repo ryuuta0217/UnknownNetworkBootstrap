bf3c9581f8c190812c6cd93ce6f003f18d7aa8d3
ryuuta0217 <ryuuta.iwakura@gmail.com>
2022-06-14 15:37:09 +0900

    Added Dispenser before shoot Event

---
+++ src/main/java/net/unknown/launchwrapper/event/BlockDispenseBeforeEvent.java
@@ -0,0 +1,55 @@
+package net.unknown.launchwrapper.event;
+
+import net.minecraft.core.BlockSource;
+import net.minecraft.world.item.ItemStack;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_18_R1.block.CraftBlock;
+import org.bukkit.craftbukkit.v1_18_R1.block.CraftBlockState;
+import org.bukkit.craftbukkit.v1_18_R1.util.CraftMagicNumbers;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class BlockDispenseBeforeEvent extends Event implements Cancellable {
+    private static final HandlerList HANDLERS = new HandlerList();
+    private boolean isCancelled = false;
+    private final BlockSource src;
+    private ItemStack item;
+
+    public BlockDispenseBeforeEvent(BlockSource src, ItemStack item) {
+        this.src = src;
+        this.item = item;
+    }
+
+    public BlockSource getBlockSource() {
+        return this.src;
+    }
+
+    public Block getBukkitBlock() {
+        return CraftBlock.at(this.src.getLevel(), this.src.getPos());
+    }
+
+    public ItemStack getItem() {
+        return this.item;
+    }
+
+    public void setItem(ItemStack newItem) {
+        this.item = newItem;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.isCancelled = cancel;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return BlockDispenseBeforeEvent.HANDLERS;
+    }
+}
---
+++ src/main/java/net/unknown/launchwrapper/mixins/MixinDefaultDispenseItemBehavior.java
@@ -0,0 +1,45 @@
+package net.unknown.launchwrapper.mixins;
+
+import net.minecraft.core.BlockSource;
+import net.minecraft.core.Direction;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.core.dispenser.DispenseItemBehavior;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.DispenserBlock;
+import net.unknown.launchwrapper.event.BlockDispenseBeforeEvent;
+import org.bukkit.Bukkit;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(DefaultDispenseItemBehavior.class)
+public abstract class MixinDefaultDispenseItemBehavior implements DispenseItemBehavior {
+    @Shadow private Direction enumdirection;
+
+    @Shadow protected abstract ItemStack execute(BlockSource pointer, ItemStack stack);
+
+    @Shadow protected abstract void playSound(BlockSource pointer);
+
+    @Shadow protected abstract void playAnimation(BlockSource pointer, Direction side);
+
+    /**
+     * Add BlockDispenseBeforeEvent
+     *
+     * @author ryuuta0217
+     */
+    @Overwrite
+    @Override
+    public final ItemStack dispense(BlockSource pointer, ItemStack stack) {
+        this.enumdirection = pointer.getBlockState().getValue(DispenserBlock.FACING); // Paper - cache facing direction
+        // Unknown Network Start - Add BlockDispenseBeforeEvent
+        BlockDispenseBeforeEvent event = new BlockDispenseBeforeEvent(pointer, stack);
+        Bukkit.getPluginManager().callEvent(event);
+        if(!event.isCancelled()) {
+            event.setItem(this.execute(event.getBlockSource(), event.getItem()));
+            this.playSound(event.getBlockSource());
+            this.playAnimation(event.getBlockSource(), enumdirection); // Paper - cache facing direction
+        }
+        return event.getItem();
+        // Unknown network End
+    }
+}
