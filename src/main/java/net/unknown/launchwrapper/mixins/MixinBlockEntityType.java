299009107986cd580441c2f3cf2aecf66f4b833e
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-05-30 07:13:00 +0900

    Registry Initialization API

---
+++ src/main/java/net/unknown/launchwrapper/mixins/MixinBlockEntityType.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2023 Unknown Network Developers and contributors.
+ *
+ * All rights reserved.
+ *
+ * NOTICE: This license is subject to change without prior notice.
+ *
+ * Redistribution and use in source and binary forms, *without modification*,
+ *     are permitted provided that the following conditions are met:
+ *
+ * I. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ * II. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * III. Neither the name of Unknown Network nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific prior written permission.
+ *
+ * IV. This source code and binaries is provided by the copyright holders and contributors "AS-IS" and
+ *     any express or implied warranties, including, but not limited to, the implied warranties of
+ *     merchantability and fitness for a particular purpose are disclaimed.
+ *     In not event shall the copyright owner or contributors be liable for
+ *     any direct, indirect, incidental, special, exemplary, or consequential damages
+ *     (including but not limited to procurement of substitute goods or services;
+ *     loss of use data or profits; or business interruption) however caused and on any theory of liability,
+ *     whether in contract, strict liability, or tort (including negligence or otherwise)
+ *     arising in any way out of the use of this source code, event if advised of the possibility of such damage.
+ */
+
+package net.unknown.launchwrapper.mixins;
+
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.unknown.launchwrapper.registry.CustomRegistryManager;
+import net.unknown.launchwrapper.registry.RegistryType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockEntityType.class)
+public abstract class MixinBlockEntityType {
+    @Inject(method = "<clinit>", at = @At("HEAD"))
+    private static void onInitialize(CallbackInfo ci) {
+        CustomRegistryManager.callInitializing(RegistryType.BLOCK_ENTITY_TYPE);
+    }
+
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void onInitialized(CallbackInfo ci) {
+        CustomRegistryManager.callInitialized(RegistryType.BLOCK_ENTITY_TYPE);
+    }
+}
