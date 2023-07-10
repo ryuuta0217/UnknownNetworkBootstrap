36f1847df1f10486b540c9f5f1ef06f9e5c40bb4
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-07-10 20:29:55 +0900

    Change EggItem Max stack size to 64.

---
+++ src/main/java/net/unknown/launchwrapper/mixins/MixinItems.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023 Unknown Network Developers and contributors.
+ *
+ * All rights reserved.
+ *
+ * NOTICE: This license is subject to change without prior notice.
+ *
+ * Redistribution and use in source and binary forms, *without modification*,
+ *     are permitted provided that the following conditions are met:
+ *
+ * I. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ * II. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * III. Neither the name of Unknown Network nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific prior written permission.
+ *
+ * IV. This source code and binaries is provided by the copyright holders and contributors "AS-IS" and
+ *     any express or implied warranties, including, but not limited to, the implied warranties of
+ *     merchantability and fitness for a particular purpose are disclaimed.
+ *     In not event shall the copyright owner or contributors be liable for
+ *     any direct, indirect, incidental, special, exemplary, or consequential damages
+ *     (including but not limited to procurement of substitute goods or services;
+ *     loss of use data or profits; or business interruption) however caused and on any theory of liability,
+ *     whether in contract, strict liability, or tort (including negligence or otherwise)
+ *     arising in any way out of the use of this source code, event if advised of the possibility of such damage.
+ */
+
+package net.unknown.launchwrapper.mixins;
+
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.unknown.launchwrapper.mixininterfaces.IMixinItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Items.class)
+public class MixinItems {
+    @Inject(method = "registerItem(Lnet/minecraft/resources/ResourceKey;Lnet/minecraft/world/item/Item;)Lnet/minecraft/world/item/Item;", at = @At("HEAD"))
+    private static void onRegisterItem(ResourceKey<Item> key, Item item, CallbackInfoReturnable<Item> cir) {
+        if (key.location().equals(ResourceLocation.of("minecraft:egg", ':')) && item instanceof IMixinItem mixinItem) {
+            mixinItem.setMaxStackSize(64); // set max stack size to 64
+        }
+    }
+}
